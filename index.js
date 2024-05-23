dce275a7d42671d4cebd086e7b1c1b1548e7d484
SomebodyBlacked <WhiteComment@protonmail.com>
2024-05-23 01:53:03 +0000

    🧠 Refactor users to json

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const app = express();
 
 const port = process.env.PORT || process.argv[2] || 3000;
 
-let users = [
-  { id: 1, nombre: "Juan", edad: 30 },
-  { id: 2, nombre: "Pedro", edad: 29 },
-  { id: 3, nombre: "Carla", edad: 31 },
-  { id: 4, nombre: "Maria", edad: 28 },
-  { id: 5, nombre: "Carlos", edad: 32 },
-  { id: 6, nombre: "Ana", edad: 27 },
-  { id: 7, nombre: "Luis", edad: 33 },
-  { id: 8, nombre: "Marta", edad: 26 },
-  { id: 9, nombre: "Silvia", edad: 34 },
-];
+const users = require("./users.json");
 
 app.get("/", (req, res) => {
   res.json("Hola mundo");
